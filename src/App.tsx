6591ee71fb156e5beffed4b16a48592a1be2b1b0
pmoise <petercmoise@gmail.com>
2023-11-29 14:28:10 -0500

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import "./index.css";
 import FoodEntry from "./components/FoodEntry";
 import haitianWhiteRice from "./assets/haitian-rice-example.png";
